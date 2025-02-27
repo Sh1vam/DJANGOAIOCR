d23d09ffc31174c84e6cee07e4afc0db444e07b4
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2025-02-27 16:51:43 +0530

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UploadExtractImageView, GetExtractedDataView
+from .views import index,UploadExtractImageView, GetExtractedDataView
 
 urlpatterns = [
+    path('', index, name="index"),
     path('upload-image/', UploadExtractImageView.as_view(), name='upload-image'),
     path('get-extracted-data/', GetExtractedDataView.as_view(), name='get-extracted-data'),
 ]
